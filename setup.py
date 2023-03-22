3d3a28c34e3c994fe550ac7a810850930da849a9
Owner <yourdatatalking@gmail.com>
2023-03-22 05:05:58 -0700

    updated files into functions, initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='real_estate',
+	version='',
+	packages=['requirements.txt'],
+	url='',
+	license='',
+	author='owner',
+	author_email='',
+	description=''
+)
